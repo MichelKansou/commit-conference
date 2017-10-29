3076b3d90f153f8d2a545c06bfbdd2f8d37e79e8
Michel Kansou <michel.kansou@outlook.fr>
2017-10-29 19:17:41 +0100

    :lipstick:
    add form button
    add responsive form

--- src/js/Views/Contact.jsx
+++ src/js/Views/Contact.jsx
@@ -6,30 +6,33 @@ export default class Contact extends Component {
             <section id='contact'>
                 <div className='container'>
                     <h1 className='title'>Nous contacter</h1>
-                    <form className='contact-form'>
-                        <div className='row'>
-                            <div className='column'>
-                                <div className='field'>
-                                    <div className='header'>Nom</div>
-                                    <input name='name' type='text' placeholder='Votre nom'/>
+                    <div className='form-container'>
+                        <form>
+                            <div className='row'>
+                                <div className='column'>
+                                    <div className='field'>
+                                        <div className='header'>Nom</div>
+                                        <input name='name' type='text' placeholder='Votre nom'/>
+                                    </div>
+                                </div>
+                                <div className='column'>
+                                    <div className='field'>
+                                        <div className='header'>Nom de la société</div>
+                                        <input name='company' type='text' placeholder='Nom de votre société'/>
+                                    </div>
                                 </div>
                             </div>
-                            <div className='column'>
-                                <div className='field'>
-                                    <div className='header'>Nom de la société</div>
-                                    <input name='company' type='text' placeholder='Nom de votre société'/>
-                                </div>
+                            <div className='field'>
+                                <div className='header'>Email</div>
+                                <input name='email' type='text' placeholder='Votre adresse email' />
                             </div>
-                        </div>
-                        <div className='field'>
-                            <div className='header'>Email</div>
-                            <input name='email' type='text' placeholder='Votre adresse email' />
-                        </div>
-                        <div className='field'>
-                            <div className='header'>Message</div>
-                            <textarea name='message' placeholder='Votre message'/>
-                        </div>
-                    </form>
+                            <div className='field'>
+                                <div className='header'>Message</div>
+                                <textarea name='message' placeholder='Votre message'/>
+                            </div>
+                        </form>
+                        <button className='btn'>Envoyer</button>
+                    </div>
                 </div>
             </section>
         );

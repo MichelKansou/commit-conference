ca9795fb758494e58d675b64eb58944587da918d
Michel Kansou <michel.kansou@outlook.fr>
2017-11-13 16:06:29 +0100

    test heruko config var

--- src/js/App.js
+++ src/js/App.js
@@ -7,13 +7,20 @@ import { createBrowserHistory } from 'history';
 import * as firebase from 'firebase';
 // import { config } from './secret/config.js';
 
+const aws = require('aws-sdk');
+
+let s3 = new aws.S3({
+  accessKeyId: process.env.APIKEY
+});
+
+
 const history = createBrowserHistory();
 
 class App extends Component {
 
     constructor(props) {
         super(props);
-        console.log(process.env.APIKEY);
+        console.log(s3);
         // firebase.initializeApp(config);
     }
 

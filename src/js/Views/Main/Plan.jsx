906984dd6a55f283132313521c4dc93d4d095adc
Michel Kansou <michel.kansou@outlook.fr>
2017-11-21 15:33:28 +0100

    :lipstick: add logo

--- src/js/Views/Main/Plan.jsx
+++ src/js/Views/Main/Plan.jsx
@@ -12,8 +12,10 @@ export default class Plan extends Component {
                             <div className='column'>
                                 <div className='container'>
                                     <div className='row'>
-                                        <img src='' className='subject-logo'></img>
-                                        <div className='column'>
+                                        <div className='image-container'>
+                                            <img class='subject-logo' src="./images/subjects logo/logo-quantum.png"/>
+                                        </div>
+                                    <div className='column'>
                                             <h3 className='subject-title'>Ordinateurs quantiques</h3>
                                             <h4 className='subject-description'>Comment assurer la pérénité des données après l'apparition des ordinateurs quantiques pour les entreprises françaises ?</h4>
                                             {
@@ -26,8 +28,10 @@ export default class Plan extends Component {
                             <div className='column'>
                                 <div className='container'>
                                     <div className='row'>
-                                        <img src='' className='subject-logo'></img>
-                                        <div className='column'>
+                                        <div className='image-container'>
+                                            <img class='subject-logo' src="./images/subjects logo/logo-reactive.png"/>
+                                        </div>
+                                    <div className='column'>
                                             <h3 className='subject-title'>Reactive System</h3>
                                             <h4 className='subject-description'>Améliorez la disponibilité de votre application avec Spring et les systèmes réactifs</h4>
                                             {
@@ -42,8 +46,10 @@ export default class Plan extends Component {
                             <div className='column'>
                                 <div className='container'>
                                     <div className='row'>
-                                        <img src='' className='subject-logo'></img>
-                                        <div className='column'>
+                                        <div className='image-container'>
+                                            <img class='subject-logo' src="./images/subjects logo/logo-iot.png"/>
+                                        </div>
+                                    <div className='column'>
                                             <h3 className='subject-title'>Internet Of Things</h3>
                                             <h4 className='subject-description'>Comment utiliser l'IoT sans compromettre la sécurité du SI ?</h4>
                                             {
@@ -56,8 +62,10 @@ export default class Plan extends Component {
                             <div className='column'>
                                 <div className='container'>
                                     <div className='row'>
-                                        <img src='' className='subject-logo'></img>
-                                        <div className='column'>
+                                        <div className='image-container'>
+                                            <img class='subject-logo' src="./images/subjects logo/logo-mixed-reality.png"/>
+                                        </div>
+                                    <div className='column'>
                                             <h3 className='subject-title'>Mixed Reality</h3>
                                             <h4 className='subject-description'>La Mixed Reality marque-t-elle une nouvelle ère du numérique? (Cas d'étude MS HoloLens, effets sur la productivité, possibilités d'évolutions)</h4>
                                             {
@@ -72,8 +80,10 @@ export default class Plan extends Component {
                             <div className='column'>
                                 <div className='container'>
                                     <div className='row'>
-                                        <img src='' className='subject-logo'></img>
-                                        <div className='column'>
+                                        <div className='image-container'>
+                                            <img class='subject-logo' src="./images/subjects logo/logo-web-semantic.png"/>
+                                        </div>
+                                    <div className='column'>
                                             <h3 className='subject-title'>Web sémantique</h3>
                                             <h4 className='subject-description'>En quoi le web sémantique permet de donner du sens aux données ?</h4>
                                             {
@@ -86,8 +96,10 @@ export default class Plan extends Component {
                             <div className='column'>
                                 <div className='container'>
                                     <div className='row'>
-                                        <img src='' className='subject-logo'></img>
-                                        <div className='column'>
+                                        <div className='image-container'>
+                                            <img src='' className='subject-logo'></img>
+                                        </div>
+                                    <div className='column'>
                                             <h3 className='subject-title'>Compétitivité des entreprises</h3>
                                             <h4 className='subject-description'>Comment moderniser son système d’information, dans le web, efficacement dans l’ère du temps tout en restant compétitif ?</h4>
                                             {
@@ -102,8 +114,10 @@ export default class Plan extends Component {
                             <div className='column'>
                                 <div className='container'>
                                     <div className='row'>
-                                        <img src='' className='subject-logo'></img>
-                                        <div className='column'>
+                                        <div className='image-container'>
+                                            <img class='subject-logo' src="./images/subjects logo/logo-evolution-si.png"/>
+                                        </div>
+                                    <div className='column'>
                                             <h3 className='subject-title'>Évolution des matériels du SI</h3>
                                             <h4 className='subject-description'>Quand renouveler le matériel du SI pour avoir le meilleur rapport coût/efficacité/eco-TIC ?</h4>
                                             {
@@ -116,8 +130,10 @@ export default class Plan extends Component {
                             <div className='column'>
                                 <div className='container'>
                                     <div className='row'>
-                                        <img src='' className='subject-logo'></img>
-                                        <div className='column'>
+                                        <div className='image-container'>
+                                            <img class='subject-logo' src="./images/subjects logo/logo-JS.png"/>
+                                        </div>
+                                    <div className='column'>
                                             <h3 className='subject-title'>Isomorphic JavaScript</h3>
                                             <h4 className='subject-description'>En quoi le JavaScript isomorphe permet d'harmoniser le système d'information afin de répondre à une demande croissante de performance ?</h4>
                                             {
@@ -132,8 +148,10 @@ export default class Plan extends Component {
                             <div className='column'>
                                 <div className='container'>
                                     <div className='row'>
-                                        <img src='' className='subject-logo'></img>
-                                        <div className='column'>
+                                        <div className='image-container'>
+                                            <img class='subject-logo' src="./images/subjects logo/logo-ethereum.png"/>
+                                        </div>
+                                    <div className='column'>
                                             <h3 className='subject-title'>Blockchain et DApps</h3>
                                             <h4 className='subject-description'>Comment la blockchain va révolutionner la manière dont on conçoit les applications web grace au DApps?</h4>
                                             {
@@ -146,8 +164,10 @@ export default class Plan extends Component {
                             <div className='column'>
                                 <div className='container'>
                                     <div className='row'>
-                                        <img src='' className='subject-logo'></img>
-                                        <div className='column'>
+                                        <div className='image-container'>
+                                            <img class='subject-logo' src="./images/subjects logo/logo-3d-printer.png"/>
+                                        </div>
+                                    <div className='column'>
                                             <h3 className='subject-title'>Impression 3D</h3>
                                             <h4 className='subject-description'>L'impression 3D peut-elle remplacer une chaîne de production ?</h4>
                                             {
@@ -162,8 +182,10 @@ export default class Plan extends Component {
                             <div className='column'>
                                 <div className='container'>
                                     <div className='row'>
-                                        <img src='' className='subject-logo'></img>
-                                        <div className='column'>
+                                        <div className='image-container'>
+                                            <img class='subject-logo' src="./images/subjects logo/logo-lifi.png"/>
+                                        </div>
+                                    <div className='column'>
                                             <h3 className='subject-title'>Le Lifi</h3>
                                             <h4 className='subject-description'>En quoi le LiFi sera un complément, corrigeant les défauts du WiFi concernant les SI en entreprises.</h4>
                                             {

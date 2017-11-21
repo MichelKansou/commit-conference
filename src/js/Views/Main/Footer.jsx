0b731a51689b3a3f08647b7f5464498f1ea78f52
Michel Kansou <michel.kansou@outlook.fr>
2017-11-21 10:57:02 +0100

    :lipstick: add Planing

--- src/js/Views/Main/Footer.jsx
+++ src/js/Views/Main/Footer.jsx
@@ -3,13 +3,13 @@ import Contact from './Contact';
 
 export default class Footer extends Component {
     render() {
+        // <a className='social-media'><i className='fa fa-twitter'></i>@Commit</a>
         return (
             <footer>
                 <Contact />
                 <div className='more-info'>
                     <a className='social-media' href='mailto:commit-event@outlook.fr'><i className='fa fa-envelope'></i>commit-event@outlook.fr</a>
-                    <a className='social-media'><i className='fa fa-twitter'></i>@Commit</a>
-                    <a className='social-media'><i className='fa fa-facebook'></i>Commit</a>
+                    <a className='social-media' href='https://www.facebook.com/Commit-1968713566717788/'><i className='fa fa-facebook'></i>Commit</a>
                 </div>
             </footer>
         );
--- src/js/Views/Main/Plan.jsx
+++ src/js/Views/Main/Plan.jsx
@@ -11,36 +11,166 @@ export default class Plan extends Component {
                         <div className='row'>
                             <div className='column'>
                                 <div className='container'>
-                                    <img src='' className='subject-logo'></img>
-                                    <h3 className='subject-title'>Ordinateurs quantiques</h3>
-                                    <h4 className='subject-description'>Comment assurer la pérénité des données après l'apparition des ordinateurs quantiques pour les entreprises françaises ?</h4>
-                                    <h5 className='subject-date'>25 janvier 2018, 13h30 - 14h30</h5>
+                                    <div className='row'>
+                                        <img src='' className='subject-logo'></img>
+                                        <div className='column'>
+                                            <h3 className='subject-title'>Ordinateurs quantiques</h3>
+                                            <h4 className='subject-description'>Comment assurer la pérénité des données après l'apparition des ordinateurs quantiques pour les entreprises françaises ?</h4>
+                                            {
+                                                 //<h5 className='subject-date'>25 janvier 2018, 13h30 - 14h30</h5>
+                                        }
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                             <div className='column'>
                                 <div className='container'>
-                                    <img src='' className='subject-logo'></img>
-                                    <h3 className='subject-title'>Reactive System</h3>
-                                    <h4 className='subject-description'>Améliorez la disponibilité de votre application avec Spring et les systèmes réactifs</h4>
-                                    <h5 className='subject-date'>25 janvier 2018, 13h30 - 14h30</h5>
+                                    <div className='row'>
+                                        <img src='' className='subject-logo'></img>
+                                        <div className='column'>
+                                            <h3 className='subject-title'>Reactive System</h3>
+                                            <h4 className='subject-description'>Améliorez la disponibilité de votre application avec Spring et les systèmes réactifs</h4>
+                                            {
+                                                 //<h5 className='subject-date'>25 janvier 2018, 13h30 - 14h30</h5>
+                                        }
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                         <div className='row'>
                             <div className='column'>
                                 <div className='container'>
-                                    <img src='' className='subject-logo'></img>
-                                    <h3 className='subject-title'>Ordinateurs quantiques</h3>
-                                    <h4 className='subject-description'>Comment assurer la pérénité des données après l'apparition des ordinateurs quantiques pour les entreprises françaises ?</h4>
-                                    <h5 className='subject-date'>25 janvier 2018, 13h30 - 14h30</h5>
+                                    <div className='row'>
+                                        <img src='' className='subject-logo'></img>
+                                        <div className='column'>
+                                            <h3 className='subject-title'>Internet Of Things</h3>
+                                            <h4 className='subject-description'>Comment utiliser l'IoT sans compromettre la sécurité du SI ?</h4>
+                                            {
+                                                 //<h5 className='subject-date'>25 janvier 2018, 13h30 - 14h30</h5>
+                                        }
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                             <div className='column'>
                                 <div className='container'>
-                                    <img src='' className='subject-logo'></img>
-                                    <h3 className='subject-title'>Reactive System</h3>
-                                    <h4 className='subject-description'>Améliorez la disponibilité de votre application avec Spring et les systèmes réactifs</h4>
-                                    <h5 className='subject-date'>25 janvier 2018, 13h30 - 14h30</h5>
+                                    <div className='row'>
+                                        <img src='' className='subject-logo'></img>
+                                        <div className='column'>
+                                            <h3 className='subject-title'>Mixed Reality</h3>
+                                            <h4 className='subject-description'>La Mixed Reality marque-t-elle une nouvelle ère du numérique? (Cas d'étude MS HoloLens, effets sur la productivité, possibilités d'évolutions)</h4>
+                                            {
+                                                 //<h5 className='subject-date'>25 janvier 2018, 13h30 - 14h30</h5>
+                                        }
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='row'>
+                            <div className='column'>
+                                <div className='container'>
+                                    <div className='row'>
+                                        <img src='' className='subject-logo'></img>
+                                        <div className='column'>
+                                            <h3 className='subject-title'>Web sémantique</h3>
+                                            <h4 className='subject-description'>En quoi le web sémantique permet de donner du sens aux données ?</h4>
+                                            {
+                                                 //<h5 className='subject-date'>25 janvier 2018, 13h30 - 14h30</h5>
+                                        }
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='column'>
+                                <div className='container'>
+                                    <div className='row'>
+                                        <img src='' className='subject-logo'></img>
+                                        <div className='column'>
+                                            <h3 className='subject-title'>Compétitivité des entreprises</h3>
+                                            <h4 className='subject-description'>Comment moderniser son système d’information, dans le web, efficacement dans l’ère du temps tout en restant compétitif ?</h4>
+                                            {
+                                                 //<h5 className='subject-date'>25 janvier 2018, 13h30 - 14h30</h5>
+                                        }
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='row'>
+                            <div className='column'>
+                                <div className='container'>
+                                    <div className='row'>
+                                        <img src='' className='subject-logo'></img>
+                                        <div className='column'>
+                                            <h3 className='subject-title'>Évolution des matériels du SI</h3>
+                                            <h4 className='subject-description'>Quand renouveler le matériel du SI pour avoir le meilleur rapport coût/efficacité/eco-TIC ?</h4>
+                                            {
+                                                 //<h5 className='subject-date'>25 janvier 2018, 13h30 - 14h30</h5>
+                                        }
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='column'>
+                                <div className='container'>
+                                    <div className='row'>
+                                        <img src='' className='subject-logo'></img>
+                                        <div className='column'>
+                                            <h3 className='subject-title'>Isomorphic JavaScript</h3>
+                                            <h4 className='subject-description'>En quoi le JavaScript isomorphe permet d'harmoniser le système d'information afin de répondre à une demande croissante de performance ?</h4>
+                                            {
+                                                 //<h5 className='subject-date'>25 janvier 2018, 13h30 - 14h30</h5>
+                                        }
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='row'>
+                            <div className='column'>
+                                <div className='container'>
+                                    <div className='row'>
+                                        <img src='' className='subject-logo'></img>
+                                        <div className='column'>
+                                            <h3 className='subject-title'>Blockchain et DApps</h3>
+                                            <h4 className='subject-description'>Comment la blockchain va révolutionner la manière dont on conçoit les applications web grace au DApps?</h4>
+                                            {
+                                                 //<h5 className='subject-date'>25 janvier 2018, 13h30 - 14h30</h5>
+                                        }
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='column'>
+                                <div className='container'>
+                                    <div className='row'>
+                                        <img src='' className='subject-logo'></img>
+                                        <div className='column'>
+                                            <h3 className='subject-title'>Impression 3D</h3>
+                                            <h4 className='subject-description'>L'impression 3D peut-elle remplacer une chaîne de production ?</h4>
+                                            {
+                                                 //<h5 className='subject-date'>25 janvier 2018, 13h30 - 14h30</h5>
+                                        }
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='row'>
+                            <div className='column'>
+                                <div className='container'>
+                                    <div className='row'>
+                                        <img src='' className='subject-logo'></img>
+                                        <div className='column'>
+                                            <h3 className='subject-title'>Le Lifi</h3>
+                                            <h4 className='subject-description'>En quoi le LiFi sera un complément, corrigeant les défauts du WiFi concernant les SI en entreprises.</h4>
+                                            {
+                                                 //<h5 className='subject-date'>25 janvier 2018, 13h30 - 14h30</h5>
+                                        }
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>

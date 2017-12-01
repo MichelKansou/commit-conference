222adcdec7ef932e6753285a743f4ff428e899cc
PerthuisQuentin <perthuis.quentin@gmail.com>
2017-12-01 11:52:50 +0100

    :lipstick: [Footer] Adding of Linkedin and Youtube links

--- src/js/Views/Main/Footer.jsx
+++ src/js/Views/Main/Footer.jsx
@@ -10,6 +10,8 @@ export default class Footer extends Component {
                 <div className='more-info'>
                     <a className='social-media' href='mailto:commit-event@outlook.fr'><i className='fa fa-envelope'></i>commit-event@outlook.fr</a>
                     <a className='social-media' rel='noopener noreferrer' target="_blank" href='https://www.facebook.com/Commit-1968713566717788/'><i className='fa fa-facebook'></i>Commit</a>
+                    <a className='social-media' rel='noopener noreferrer' target="_blank" href='https://www.linkedin.com/company/11354391/'><i className='fa fa-linkedin'></i>Commit</a>
+                    <a className='social-media' rel='noopener noreferrer' target="_blank" href='https://www.youtube.com/channel/UCgGT2AN81wppgzfyw_RuS5g'><i className='fa fa-youtube'></i>Commit Event Arras</a>
                 </div>
             </footer>
         );
